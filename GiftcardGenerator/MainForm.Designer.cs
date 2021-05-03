f5fe5117f669e608171a3a3219b5c0b0a0a61664
SinisterSpatula <49018201+SinisterSpatula@users.noreply.github.com>
2021-05-02 20:58:31 -0500

    Update MainForm.Designer.cs

--- GiftcardGenerator/MainForm.Designer.cs
+++ GiftcardGenerator/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace GiftcardGenerator
+namespace GiftcardGenerator
 {
     partial class MainForm
     {
@@ -30,13 +30,18 @@
         {
             this.cbCardType = new System.Windows.Forms.ComboBox();
             this.btnGenerate = new System.Windows.Forms.Button();
-            this.lbGiftcardNumbers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.money = new System.Windows.Forms.NumericUpDown();
+            this.lbGiftcardNumbers = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.money)).BeginInit();
             this.SuspendLayout();
             // 
             // cbCardType
             // 
             this.cbCardType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbCardType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbCardType.FormattingEnabled = true;
             this.cbCardType.Items.AddRange(new object[] {
             "Apple",
@@ -44,48 +49,93 @@
             "Google Play",
             "Target",
             "Walmart"});
-            this.cbCardType.Location = new System.Drawing.Point(12, 12);
+            this.cbCardType.Location = new System.Drawing.Point(16, 36);
+            this.cbCardType.Margin = new System.Windows.Forms.Padding(4);
             this.cbCardType.Name = "cbCardType";
-            this.cbCardType.Size = new System.Drawing.Size(342, 21);
+            this.cbCardType.Size = new System.Drawing.Size(320, 28);
             this.cbCardType.Sorted = true;
             this.cbCardType.TabIndex = 0;
             // 
             // btnGenerate
             // 
             this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnGenerate.Location = new System.Drawing.Point(360, 12);
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(460, 31);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(4);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(75, 21);
+            this.btnGenerate.Size = new System.Drawing.Size(119, 36);
             this.btnGenerate.TabIndex = 1;
             this.btnGenerate.Text = "Generate";
             this.btnGenerate.UseVisualStyleBackColor = true;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Brand";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(339, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Money Value";
+            // 
+            // money
+            // 
+            this.money.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.money.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.money.Location = new System.Drawing.Point(343, 36);
+            this.money.Maximum = new decimal(new int[] {
+            9000,
+            0,
+            0,
+            0});
+            this.money.Name = "money";
+            this.money.Size = new System.Drawing.Size(101, 27);
+            this.money.TabIndex = 5;
+            this.money.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
             // lbGiftcardNumbers
             // 
-            this.lbGiftcardNumbers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lbGiftcardNumbers.Font = new System.Drawing.Font("Consolas", 14F);
-            this.lbGiftcardNumbers.FormattingEnabled = true;
-            this.lbGiftcardNumbers.ItemHeight = 22;
-            this.lbGiftcardNumbers.Location = new System.Drawing.Point(12, 39);
+            this.lbGiftcardNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbGiftcardNumbers.Location = new System.Drawing.Point(13, 82);
             this.lbGiftcardNumbers.Name = "lbGiftcardNumbers";
-            this.lbGiftcardNumbers.Size = new System.Drawing.Size(423, 180);
-            this.lbGiftcardNumbers.TabIndex = 2;
+            this.lbGiftcardNumbers.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.lbGiftcardNumbers.Size = new System.Drawing.Size(574, 289);
+            this.lbGiftcardNumbers.TabIndex = 6;
+            this.lbGiftcardNumbers.Text = "";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 223);
+            this.ClientSize = new System.Drawing.Size(599, 383);
             this.Controls.Add(this.lbGiftcardNumbers);
+            this.Controls.Add(this.money);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.cbCardType);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.Text = "Giftcard Generator";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.money)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -93,7 +143,10 @@
 
         private System.Windows.Forms.ComboBox cbCardType;
         private System.Windows.Forms.Button btnGenerate;
-        private System.Windows.Forms.ListBox lbGiftcardNumbers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown money;
+        private System.Windows.Forms.RichTextBox lbGiftcardNumbers;
     }
 }
 
