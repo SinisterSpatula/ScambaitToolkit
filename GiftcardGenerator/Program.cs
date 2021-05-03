d5f8e1edf8818fe941365073e7e52ef1fbecf297
SinisterSpatula <49018201+SinisterSpatula@users.noreply.github.com>
2021-05-02 21:03:09 -0500

    Update Program.cs

--- GiftcardGenerator/Program.cs
+++ GiftcardGenerator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,5 +18,7 @@ namespace GiftcardGenerator
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
+
+
     }
 }
